3e2f72d88695f714d48e6cbb523d8109bdade1be
Febryo Agung <febryo.agung@gmail.com>
2021-06-25 00:57:18 -0700

    start mono flux basic

---
+++ src/main/java/guru/springframework/reactiveexample/PersonRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.reactiveexample;
+
+import guru.springframework.reactiveexample.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PersonRepository {
+    Mono<Person> getById(Integer id);
+    Flux<Person> findAll();
+}
